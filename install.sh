53bdf5fedaf19597daf1ad1392b91a2139e2a478
Michelle M. Ludvigsen <michelleludvigsen@gmail.com>
2016-10-17 19:41:40 +0200

    i3: add prompt for which i3 config to stow

--- install.sh
+++ install.sh
@@ -4,5 +4,16 @@ stow -t ~ tmux
 stow -t ~ vim
 stow -t ~ profile
 stow -t ~ zsh
-stow -t ~ i3
 stow -t ~ git
+
+# i3 window manager specific
+cd i3;
+
+echo "i3: laptop or work setup?"
+select answer in "laptop" "work"; do
+    case $answer in
+        laptop ) stow -t ~ laptop; exit;;
+        work ) stow -t ~ work; exit;;
+    esac
+done
+
